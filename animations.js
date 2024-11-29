5efa160c51221401c95e7cb34609cc48894555c7
benjnz <84190930+benjnz@users.noreply.github.com>
2024-11-29 23:39:04 +0000

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,18 +1,27 @@
-// Fade-in Animations
-window.addEventListener("DOMContentLoaded", () => {
-  gsap.from(".fade-in", {
-    duration: 1,
+// Animate page elements with GSAP
+document.addEventListener("DOMContentLoaded", () => {
+  // Fade-in for the hero section
+  gsap.from("header .hero h1", {
     opacity: 0,
-    y: 50,
-    stagger: 0.3,
-    ease: "power1.out",
+    y: -20,
+    duration: 1,
+    ease: "power2.out",
   });
 
-  gsap.from(".project.card", {
+  gsap.from("header .hero p", {
+    opacity: 0,
+    y: 20,
     duration: 1,
+    delay: 0.3,
+    ease: "power2.out",
+  });
+
+  // Staggered animation for gallery cards
+  gsap.from(".project-card", {
     opacity: 0,
     scale: 0.9,
     stagger: 0.2,
-    ease: "power1.out",
+    duration: 1,
+    ease: "power2.out",
   });
 });
