548ec8877643cde7c1d3acc8ecda87ee281cb2ab
benjnz <84190930+benjnz@users.noreply.github.com>
2024-12-20 23:14:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 console.log("THREE is:", THREE);
 
 // Check if GLTFLoader is accessible
