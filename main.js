08393bf704dbc705bc481e90b3f13c47fcdd50e1
benjnz <84190930+benjnz@users.noreply.github.com>
2025-01-01 23:10:32 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const logo = document.querySelector(".logo-img");
 
   logo.addEventListener("mouseover", () => {
-    logo.src = logo.getAttribute("data-animated"); // Switch to animated GIF
+    // Add a unique query string to force replay of the GIF
+    const animatedGif = `${logo.getAttribute("data-animated")}?t=${new Date().getTime()}`;
+    logo.src = animatedGif;
   });
 
   logo.addEventListener("mouseout", () => {
-    logo.src = logo.getAttribute("data-static"); // Revert to static frame
+    // Revert to the static frame
+    logo.src = logo.getAttribute("data-static");
   });
 });
