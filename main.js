6ed62a0ba8ee7e7c4220fb36066d9426d3190ad9
benjnz <84190930+benjnz@users.noreply.github.com>
2025-01-04 23:57:21 +0000

    Update main.js

--- main.js
+++ main.js
@@ -16,4 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
       logo.classList.remove("playing");
     }, duration);
   });
+
+  setInterval(function() {
+  var gifImage = document.getElementById('gifImage');
+  var src = gifImage.src; // Get the current source
+
+  // Refresh the GIF by appending a random query parameter to the URL
+  gifImage.src = ''; // Reset the image source
+  gifImage.src = src + '?' + new Date().getTime(); // Reassign the same source with a unique timestamp to force reload
+}, 10000); // 10000 ms = 10 seconds
+
+  
 });
