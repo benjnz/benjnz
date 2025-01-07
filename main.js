ba1729d16f91a35588d921916d6095844a70ba9d
benjnz <84190930+benjnz@users.noreply.github.com>
2025-01-07 01:18:56 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+  window.addEventListener("load", () => {
+    const loadingScreen = document.getElementById("loading-screen");
+    loadingScreen.style.opacity = "0";
+    setTimeout(() => {
+      loadingScreen.style.display = "none";
+    }, 500); // Delay to ensure smooth fade-out
+  });
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const logo = document.querySelector(".logo-img");
 
